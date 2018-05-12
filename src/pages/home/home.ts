688d210eb6c6172db2c154c18bc016ebb20359bb
rasor <soren@rasor.dk>
2018-05-12 07:53:54 +0200

    Rotation added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { NavController, AlertController, Platform } from 'ionic-angular';
 //import infamous from 'infamous'
 declare var infamous: any; 
@@ -7,18 +7,29 @@ declare var infamous: any;
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  @ViewChild("inode") inode: ElementRef;
 
   constructor(public navCtrl: NavController, private alertCtrl: AlertController, private platform: Platform) {
     // invoke infamous https://infamous.io/docs/install.html
     infamous.html.useDefaultNames();
   }
 
+  ngAfterViewInit(): void {
+    // sample from https://infamous.io/docs/workflows.html
+    const {Motor} = infamous.core
+    let node: any;
+    //node = document.querySelector('i-node')
+    node = this.inode.nativeElement;
+    console.log('Hello ' + node.tagName);
+    node.rotation = ( x, y, z ) => [ x, ++y, z ] //   
+  }
+
   // https://devdactic.com/deploy-ionic-website-heroku/
   showPlatform() {
     let text = 'I run on: ' + this.platform.platforms();
     let alert = this.alertCtrl.create({
-      title: 'My Home',
+      title: 'Home',
       subTitle: text,
       buttons: ['Ok']
     });
