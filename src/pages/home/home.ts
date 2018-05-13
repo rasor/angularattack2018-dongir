68aa08df97e5ac1f62bbe1e93db2669f9d44fbc7
rasor <soren@rasor.dk>
2018-05-13 22:10:33 +0200

    intMaze added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,7 +10,7 @@ declare var infamous: any;
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage implements AfterViewInit {
+export class HomePage implements AfterViewInit { 
   /* Content of maze
   * 0 = isle
   * 1 = wall
@@ -29,9 +29,13 @@ export class HomePage implements AfterViewInit {
     boxSize: 50,
     floorSize: {}
   };
-  
+  // maze walls for view - filled from infamousMaze.maze
   wallVectorPositions: Vector3[];
 
+  // maze like extMaze, but rows and cols are swapped to enable easier conversion to infamousMaze.maze
+  //intMaze: ExtMaze;
+
+  // error response from service
   error: any;
 
   constructor(public navCtrl: NavController, public mazeSvc: MazeProvider) {
@@ -53,10 +57,74 @@ export class HomePage implements AfterViewInit {
   private getMazeData_GotData(data: ExtMaze): void {
     // clone the data object, using its known ExtMaze shape
     let extMaze = { ...data };
-    this.getMazeData_PrepareMazeData(extMaze);
+    this.getMazeData_ConvertMazeData(extMaze);
   }
 
-  private getMazeData_PrepareMazeData(extMaze: ExtMaze):void{
+  private getMazeData_ConvertMazeData(extMaze: ExtMaze):void{
+    let intRows = extMaze.size[0]
+    let intCols = extMaze.size[1]
+
+    // maze like extMaze, but rows and cols are swapped to enable easier conversion to infamousMaze.maze
+    let intMaze: ExtMaze = {
+      pony: [],
+      domokun: [],
+      endPoint: [],
+      data: [[]],
+      size: [intCols,intRows],
+      difficulty: extMaze.difficulty,
+      mazeId: extMaze.mazeId,
+      gameState: extMaze.gameState
+    }
+    // Swap cols with rows
+    let extIndex: number; 
+    let intIndex: number = 0;
+    let currExtData: string[];
+    let extDomokun = extMaze.domokun[0];
+    let extPony = extMaze.pony[0];
+    //let extEndPoint = extMaze.endPoint[0];
+    for (let intRow = 0; intRow < intRows; intRow++) {
+      for (let intCol = 0; intCol < intCols; intCol++) {
+        // Calculate extindex for introw 0 intcol 0
+        // 0,0;         (((25 - 1) * 15)      + 0       -      (0 * 15)      = 360
+        // 0,1:         (((25 - 1) * 15)      + 0       -      (1 * 15)      = 345
+        // 1,0;         (((25 - 1) * 15)      + 1       -      (0 * 15)      = 361
+        extIndex = (((intCols - 1) * intRows) + intRow) - (intCol * intRows);
+        // Fetch data
+        currExtData = extMaze.data[extIndex];
+        // Save in new order
+        intMaze.data.push(currExtData);
+
+        // Check if extIndex is one of the figures and save new index
+        switch (extIndex) {
+          case extDomokun:
+            intMaze.domokun = [intIndex];
+            break;
+          case extPony:
+            intMaze.pony = [intIndex];
+            break;
+          // case extEndPoint:
+          //   intMaze.endPoint = [intIndex];
+          //   break;
+          default:
+            break;
+        }
+
+        // Next index
+        intIndex++;
+      }
+    }
+
+    //this.intMaze = intMaze;
+
+    // Set Wall corners - every 2nd tile in every 2nd row
+ 
+    //this.infamousMaze.rows = (intRows * 2) + 1;
+    //this.infamousMaze.cols = (intCols * 2) + 1;
+
+    this.getMazeData_PrepareMazeData();
+  } 
+
+  private getMazeData_PrepareMazeData():void{
     // Validate initial maze data
     if (this.infamousMaze.rows * this.infamousMaze.cols > this.infamousMaze.maze.length) {
       let errMsg = 'Too few data in infamousMaze.maze[]';
@@ -112,7 +180,7 @@ export class HomePage implements AfterViewInit {
     let node: any;
     node = this.flybox.nativeElement; // document.querySelector('i-node')
     console.log('Hello ' + node.tagName);
-    //node.rotation = ( x, y, z ) => [ ++x, ++y, ++z ] //   
+    node.rotation = ( x, y, z ) => [ ++x, ++y, ++z ] //   
   }
 
 }
